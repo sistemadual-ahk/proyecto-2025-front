b2b4658449e432ace81ff95bd394beb31d5770d5
Timberliine <mikeas.2004.rami.rez@gmail.com>
2025-08-08 11:47:14 -0300

    update screens
    ACTUALIZACION DE RUTAS Y ELIMINACION DE WELCOME Y LOGIN

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
-import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: WelcomeComponent },
-  { path: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent }
 ];
