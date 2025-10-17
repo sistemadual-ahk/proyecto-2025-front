86ae776e0ae93befca906286b27e03644c6bf375
Fernandita <baezfernanda2005@gmail.com>
2025-10-17 14:04:48 -0300

    operacion epica

--- src/models/operacion.model.ts
+++ src/models/operacion.model.ts
@@ -2,7 +2,7 @@ export interface Operacion {
   _id?: string;
   tipo: 'Ingreso' | 'Egreso';
   monto: number;
-  billeteraId: string;
+  billeteraId?: string;
   categoriaId: string;
   descripcion?: string;
   fecha: string;
