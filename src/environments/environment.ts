0c7849473b7e039257786e2a40c7d8a413bfa118
Mauricio Falcon <mauriciofalcon1304@gmail.com>
2025-12-17 16:19:12 -0300

    Switch API_BACK_URL to production endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  // API_BACK_URL: 'https://gastify.onrender.com/api',
-  API_BACK_URL: 'http://localhost:3000/api',
+  API_BACK_URL: 'https://gastify.onrender.com/api',
+  //API_BACK_URL: 'http://localhost:3000/api',
 };
